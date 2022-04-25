b038f36ccc85cf2dba675cdce94cb6e2669f7827
Deemantha18Dissanayaka <deemanthadissanayaka18@gmail.com>
2022-04-25 22:38:35 +0530

    implementin login controll

---
+++ src/controllers/login.contorller.js
@@ -0,0 +1,99 @@
+const Admin = require("../model/SystemAdminModel");
+const Teacher = require("../model/TeacherModel");
+const Student = require("../model/StudentAccountModel");
+const bcryptjs = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const LocalStorage = require("node-localstorage").LocalStorage;
+localStorage = new LocalStorage('./scratch');
+const {loginValidation} = require("../validations/loginValidation");
+
+const login = async (req,res,next) => {
+    const {error} = loginValidation(req.body);
+
+    if(error) {
+        res.send({message: error["details"][0]["message"]});
+    }
+
+    const adminExist = await Admin.findOne({username: req.body.username});
+    const teacherExist = await Teacher.findOne({username: req.body.username});
+    const studentExist = await Student.findOne({username: req.body.username});
+    
+    //if user is an admin
+    if(adminExist) {
+        localStorage.setItem("isAdmin", adminExist.isAdmin);
+        localStorage.setItem("isTeacher", false);
+        localStorage.setItem("isStudent", false);
+        console.log("Admin");
+        console.log(localStorage.getItem("isAdmin"));
+
+        //decrypt the password
+        const passwordValidation = await bcryptjs.compare(req.body.password, adminExist.password);
+        if(!passwordValidation) {
+            res.status(400).send({message: "Wrong password"});
+        }
+        
+        //generate json web token
+        const token = jwt.sign({_id: adminExist.id}, process.env.TOKEN_SECRET);
+        res.header("auth-token", token).send({'auth-token':token});
+
+    }
+    
+    else if(teacherExist) { //if user teacher
+        localStorage.setItem("isTeacher", teacherExist.isTeacher);
+        localStorage.setItem("isAdmin", false);
+        localStorage.setItem("isStudent", false);
+        console.log("Teacher");
+        console.log(localStorage.getItem("isTeacher"));
+
+        //dcrypt password
+        const passwordValidation = await bcryptjs.compare(req.body.password, teacherExist.password);
+        if(!passwordValidation) {
+            res.status(400).send({message: "Wrong password"});
+        }
+
+        //generate json web token
+        const token = jwt.sign({_id: teacherExist.id}, process.env.TOKEN_SECRET);
+        res.header("auth-token", token).send({"auth-token":token});
+
+    }
+    else if(studentExist) { //if user student
+        localStorage.setItem("isStudent", studentExist.isStudent);
+        localStorage.setItem("isAdmin", false);
+        localStorage.setItem("isTeacher", false);
+        console.log("Student");
+        console.log(localStorage.getItem("isStudent"));
+
+        //dcrypt password
+        const passwordValidation = await bcryptjs.compare(req.body.password, studentExist.password);
+        if(!passwordValidation) {
+            res.status(400).send({message: "Wrong Password"});
+        }
+
+        //generate json web token
+        const token = jwt.sign({_id: studentExist.id}, process.env.TOKEN_SECRET);
+        res.header("auth-token", token).send({"auth-token":token});
+    }
+    else {
+        return res.status(400).send({message: "User does not exist"});
+    }
+};
+
+let refreashTokens = [];
+
+const logout = async (req,res) => {
+    const refreashToken = req.body.token;
+
+    localStorage.removeItem("isAdmin");
+    localStorage.removeItem("isTeacher");
+    localStorage.removeItem("isStudent");
+
+    try {    
+        refreashTokens = refreashTokens.filter((token) => token !== refreashToken);
+        res.status(200).json("You are logged out successfully");
+    }
+    catch(err) {
+        return res.status(400).send({message: err});
+    }
+}
+
+module.exports = {login, logout};
